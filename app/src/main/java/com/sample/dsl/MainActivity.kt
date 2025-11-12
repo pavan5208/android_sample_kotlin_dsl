0634082e05ea5143b3725ce37349a8dc62df6cf2
satyapavan <pavan.careers5208@gmail.com>
2025-11-12 14:15:15 +0530

    test commit

--- app/src/main/java/com/sample/dsl/MainActivity.kt
+++ app/src/main/java/com/sample/dsl/MainActivity.kt
@@ -2,7 +2,7 @@ package com.sample.dsl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
